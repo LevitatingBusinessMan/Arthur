f7f8ef77f50e89b90f0a123d0eab672d082e5fee
LevitatingBusinessMan <public@reinfernhout.xyz>
2018-10-18 14:30:53 +0200

    command descriptions
    and new regex

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const puppeteer = require("puppeteer"),
 settings = require("./settings.json"),
 readline = require("readline"),
+columnify = require("columnify"),
 program = require("commander");
 
 program
@@ -48,131 +49,160 @@ puppeteer.launch(config).then(async browser => {
 
     let commands = {};
     
-    commands.rotate = (args) => new Promise ((resolve, reject) => {
+    commands.rotate = {
+        fn: (args) => new Promise ((resolve, reject) => {
 
-        if (!args.length || isNaN(args[0]))
-            resolve(console.log("\nPlease use the syntax\n>front %tab_index%\n"));
+            if (!args.length || isNaN(args[0]))
+                resolve(console.log("\nPlease use the syntax\n>front %tab_index%\n"));
 
-        console.log(`Starting rotation with interval: ${args[0]}ms`);
+            console.log(`Starting rotation with interval: ${args[0]}ms`);
 
-        let index = 0;
-        clearInterval(interval);
-        interval = setInterval(() => {
-            browser.pages().then(pages => {
-                pages[index].bringToFront()
+            let index = 0;
+            clearInterval(interval);
+            interval = setInterval(() => {
+                browser.pages().then(pages => {
+                    pages[index].bringToFront()
+            
+                    index++;
+                    if (index >= pages.length)
+                        index = 0;
+            
+                    pages[index].reload()
+                });
+            }, args[0])
         
-                index++;
-                if (index >= pages.length)
-                    index = 0;
-        
-                pages[index].reload()
-            });
-        }, args[0])
-    
-        resolve();
-    });
-
-    commands.pause = (args) => new Promise((resolve, reject) => {
-        console.log("Pausing rotation");
-        clearInterval(interval);
-
-        resolve();
-    });
-
-    commands.exit = (args) => new Promise((resolve, reject) => {
-        process.exit();
-    }); 
-
-    commands.view = (args) => new Promise((resolve, reject) => {
-
-        if (args.length < 2) {
-            browser.pages().then(pages => {
-                console.log("resetting viewport");
-                view = {width: settings.width, height: settings.height};
-                pages.forEach(p => p.setViewport(view));
-                resolve();
-            });
-        } else {
-            for (let i = 0; i < args.length; i++) {
-                if(isNaN(args[i]))
-                    return resolve(console.log("\nPlease use the syntax\nview %width% %height%\n"))
-            }
-    
-            browser.pages().then(pages => {
-                view = {width:parseInt(args[0]), height:parseInt(args[1])};
-                pages.forEach(p => p.setViewport(view));
-                resolve();
-            });
-        }
-    });
-
-    commands.pages = (args) => new Promise(async (resolve, reject) => {
-        const pages = await browser.pages();
-        for (let i = 0; i < pages.length; i++) {
-            const p = pages[i];
-            const url = p.url();
-
-            console.log(`Tab[${i+1}] ${url}`)
-        }
-        resolve();
-    });
-
-    const url_regex = /(((http)|(https)):\/\/)?(\w+:)?([a-z0-9@]+\.)+[a-z0-9]+(:\d+)?((\/[\w()?=&#-%-\S]+)+)?/g;
-    commands.add = (args) => new Promise(async (resolve, reject) => {
-        if (!args.length)
-            resolve(console.log("\nPlease use the syntax\nadd %url%\n"));
-        
-        else if (!(url_regex).test(args[0]))
-            resolve(console.log("Please only use full urls"))
-
-        else browser.newPage().then(p => {
-            p.waitForNavigation( {timeout: 0});
-            p.goto(args[0]);
-            p.setViewport(view);
             resolve();
-        });
-    });
+        }),
+        description: "Rotate tabs at set interval"
+    }
 
-    commands.close = (args) => new Promise(async (resolve, reject) => {
-        if (!args.length || isNaN(args[0]))
-            resolve(console.log("\nPlease use the syntax\n>close %tab_index%\n"));
+
+    commands.pause = {
+        fn: (args) => new Promise((resolve, reject) => {
+            console.log("Pausing rotation");
+            clearInterval(interval);
+
+            resolve();
+        }),
+        description: "Pause rotation"
+    }
+
+    commands.exit = {
+        fn: (args) => new Promise((resolve, reject) => {
+            process.exit();
+        }),
+        description: "End process"
+    } 
+
+    commands.view = {
+        fn: (args) => new Promise((resolve, reject) => {
+            if (args.length < 2) {
+                browser.pages().then(pages => {
+                    console.log("resetting viewport");
+                    view = {width: settings.width, height: settings.height};
+                    pages.forEach(p => p.setViewport(view));
+                    resolve();
+                });
+            } else {
+                for (let i = 0; i < args.length; i++) {
+                    if(isNaN(args[i]))
+                        return resolve(console.log("\nPlease use the syntax\nview %width% %height%\n"))
+                }
         
-            else browser.pages().then(pages => {
-                const index = args[0]-1;
+                browser.pages().then(pages => {
+                    view = {width:parseInt(args[0]), height:parseInt(args[1])};
+                    pages.forEach(p => p.setViewport(view));
+                    resolve();
+                });
+            }
+        }),
+        description: "Change viewport on all pages"
+    }
 
-                if (!pages[index])
-                    return resolve(console.log("Invalid tab index!\n"));
+    commands.pages = {
+        fn: (args) => new Promise(async (resolve, reject) => {
+            const pages = await browser.pages();
+            for (let i = 0; i < pages.length; i++) {
+                const p = pages[i];
+                const url = p.url();
+
+                const active = await p.evaluate("document.visibilityState") === "visible";
                 
-                console.log(`Closing page ${args[0]}:${pages[index].url()}`)
-                pages[index].close();
-                resolve();
-        });
-    });
+                console.log((active?"=> ":"   ") + `Tab[${i+1}] ${url}`);
+            }
+            resolve();
+        }),
+        description: "Show all tabs"
+    }
 
-    commands.help = (args) => new Promise(async (resolve, reject) => {
-        console.log("\nCommands:")
-        for (name in commands) {
-            console.log(name)
-        }
-        console.log("\n")
-        resolve();
-    });
+    const url_regex = /((https?|ftp):\/\/)?(([^:\n\r]+):([^@\n\r]+)@)?((www\.)?([^/\n\r]+))\/?([^?\n\r]+)?\??([^#\n\r]*)?#?([^\n\r]*)/g;
+    commands.add = {
+        fn: (args) => new Promise(async (resolve, reject) => {
+            if (!args.length)
+                return resolve(console.log("\nPlease use the syntax\nadd %url%\n"));
+            
+            if (!(url_regex).test(args[0]))
+                return resolve(console.log("Please only use full urls"))
+
+            browser.newPage().then(p => {
+                p.waitForNavigation( {timeout: 0});
+                p.goto(args[0]);
+                p.setViewport(view);
+                resolve();
+            });
+        }),
+        description: "Add a page"
+    }
+
+    commands.close = {
+        fn: (args) => new Promise(async (resolve, reject) => {
+            if (!args.length || isNaN(args[0]))
+                resolve(console.log("\nPlease use the syntax\n>close %tab_index%\n"));
+            
+                else browser.pages().then(pages => {
+                    const index = args[0]-1;
+
+                    if (!pages[index])
+                        return resolve(console.log("Invalid tab index!\n"));
+                    
+                    console.log(`Closing page ${args[0]}:${pages[index].url()}`)
+                    pages[index].close();
+                    resolve();
+            })
+        }),
+        description: "Close a page"
+    }
     
-    commands.front = (args) => new Promise(async (resolve, reject) => {
-        if (!args.length || isNaN(args[0]))
-            resolve(console.log("\nPlease use the syntax\n>front %tab_index%\n"));
-        
-            else browser.pages().then(pages => {
-                const index = args[0]-1;
+    commands.front = {
+        fn: (args) => new Promise(async (resolve, reject) => {
+            if (!args.length || isNaN(args[0]))
+                resolve(console.log("\nPlease use the syntax\n>front %tab_index%\n"));
+            
+                else browser.pages().then(pages => {
+                    const index = args[0]-1;
 
-                if (!pages[index])
-                    return resolve(console.log("Invalid tab index!\n"));
-                
-                console.log(`Bringing page ${args[0]}:${pages[index].url()} to front`)
-                pages[index].bringToFront();
-                resolve();
-        });
-    });
+                    if (!pages[index])
+                        return resolve(console.log("Invalid tab index!\n"));
+                    
+                    console.log(`Bringing page ${args[0]}:${pages[index].url()} to front`)
+                    pages[index].bringToFront();
+                    resolve();
+            });
+        }),
+        description: "Make a tab active"
+    }
+
+    commands.help = {
+        fn: (args) => new Promise(async (resolve, reject) => {
+            let list = {};
+            for (name in commands) {
+                list[name] = commands[name].description
+            }
+            console.log("\n"+columnify(list,{columns: ['COMMAND', 'DESCRIPTION']})+"\n")
+            resolve();
+        }),
+        description: "Show this list"
+    }
 
     function inpuHandler(line) {
         let args = line.split(/\s/g);
@@ -181,7 +211,7 @@ puppeteer.launch(config).then(async browser => {
 
 
         if (commands[cmd]) {
-            commands[cmd](args).then(() => rl.prompt(">"))
+            commands[cmd].fn(args).then(() => rl.prompt(">"))
         } else {
             console.log("Command not found");
             rl.prompt(">");
